8f03961982fae5ce894f301d2c04a1d929526b1e
JoaoVitorAR <jvalves1998@outlook.com.br>
2022-11-03 17:59:27 -0300

    Redirecionamento de rotas e integração com o back end

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,10 +1,13 @@
 import { VStack, Icon} from "native-base";
 import { Octicons } from '@expo/vector-icons'
+import { useNavigation  } from '@react-navigation/native'
 
 import { Header } from "../components/Header";
 import { Button } from "../components/Button";
 
 export function Pools() {
+    const { navigate } = useNavigation();
+
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Meus bolões"/>
@@ -12,7 +15,8 @@ export function Pools() {
             <VStack mt={6} mx={5} alignItems="center" borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
                 <Button 
                     title="BUSCAR BOLÃO POR CÓDIGO" 
-                    leftIcon={<Icon as={Octicons} name="search" color="balck" size="md" />}    
+                    leftIcon={<Icon as={Octicons} name="search" color="balck" size="md" />}
+                    onPress={() => navigate('find')}
                 /> 
             </VStack>
             
